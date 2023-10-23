1ed68c49adcceb0548a600991d42a2319667e2fb
faissal17 <faissalaoukacha@gmail.com>
2023-10-23 15:07:47 +0100

    define routes using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import landingPage from "./component/home/landingPage";
+import login from "./component/auth/login";
+import register from "./component/auth/register";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={landingPage} />
+          <Route path="/auth/login" Component={login} />
+          <Route path="/auth/register" Component={register} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
---
+++ src/component/home/landingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route } from "react-router-dom";
+function landingPage() {
+  return (
+    <React.Fragment>
+      <h1>Welcom to AlloMedia</h1>
+      <h3>Make your call and recive your order</h3>
+    </React.Fragment>
+  );
+}
+export default landingPage;
